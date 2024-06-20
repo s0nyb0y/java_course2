f5909ae09730cba68158e8c8b99abdfd1ab60c22
Radik Akhmetgareev <driveclub228@gmail.com>
2024-06-20 12:57:56 +0300

    lesson57 multithreading wait and notify

---
+++ src/lesson57_multithreading_wait_notify/WaitNotifyEx.java
@@ -0,0 +1,74 @@
+package lesson57_multithreading_wait_notify;
+
+public class WaitNotifyEx {
+    public static void main(String[] args) {
+        Market market = new Market();
+        Producer producer = new Producer(market);
+        Consumer consumer = new Consumer(market);
+        Thread thread1 = new Thread(producer);
+        Thread thread2 = new Thread(consumer);
+        thread1.start();
+        thread2.start();
+    }
+}
+
+class Market {
+    private int breadCount = 0;
+
+    public synchronized void getBread() {
+        while (breadCount < 1) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        breadCount--;
+        System.out.println("Буханка хлеба продана");
+        System.out.println("Количество хлеба в магазине = " + breadCount);
+        notify();
+    }
+
+    public synchronized void putBread() {
+        while (breadCount >= 5) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        breadCount++;
+        System.out.println("буханка хлеба добавлена");
+        System.out.println("Количество хлеба в магазине = " + breadCount);
+        notify();
+    }
+}
+
+class Producer implements Runnable {
+    Market market;
+
+    public Producer(Market market) {
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.putBread();
+        }
+    }
+}
+class Consumer implements Runnable {
+    Market market;
+
+    public Consumer(Market market) {
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.getBread();
+        }
+    }
+}
---
+++ src/lesson57_multithreading_wait_notify/WaitNotifyEx2.java
@@ -0,0 +1,79 @@
+package lesson57_multithreading_wait_notify;
+
+public class WaitNotifyEx2 {
+    public static void main(String[] args) {
+        Market2 market = new Market2();
+        Producer2 producer = new Producer2(market);
+        Consumer2 consumer = new Consumer2(market);
+        Thread thread1 = new Thread(producer);
+        Thread thread2 = new Thread(consumer);
+        thread1.start();
+        thread2.start();
+    }
+}
+
+class Market2 {
+    static final Object lock = new Object();
+    private int breadCount = 0;
+
+    public void putBread() {
+        synchronized (lock) {
+            while (breadCount >= 5) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            breadCount++;
+            System.out.println("Буханка хлеба добавлена");
+            System.out.println("Количество хлеба в магазине = " + breadCount);
+            lock.notify();
+        }
+    }
+
+    public void getBread() {
+        synchronized (lock) {
+            while (breadCount < 1) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            breadCount--;
+            System.out.println("Буханка хлеба продана");
+            System.out.println("Количество хлеба в магазине = " + breadCount);
+            lock.notify();
+        }
+    }
+}
+
+class Producer2 implements Runnable {
+    Market2 market;
+
+    public Producer2(Market2 market) {
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.putBread();
+        }
+    }
+}
+class Consumer2 implements Runnable {
+    Market2 market;
+
+    public Consumer2(Market2 market) {
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.getBread();
+        }
+    }
+}
